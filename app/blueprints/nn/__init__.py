8ec96d41fb29ea50e81a94a0dbe634e88d64206c
dsizo <dsizov@rpa-robin.ru>
2022-06-13 14:16:02 +0300

    Передача файла тесты

--- app/blueprints/nn/__init__.py
+++ app/blueprints/nn/__init__.py
@@ -1,7 +1,10 @@
+import os.path
+
 from flask import jsonify, request, make_response
 from flask_restx import Namespace, Resource, fields
 from utils.create_spectrogram import get_sample
 from utils.predict import create_result, get_argmax_elem_name
+from werkzeug.utils import secure_filename
 
 namespace = Namespace('nn', 'Neural network endpoints')
 
@@ -11,16 +14,20 @@ class Predict(Resource):
     @namespace.response(500, 'Internal Server error')
     @namespace.doc('Predict')
     def get(self):
-        # audio_file = request.files['bird']
-        # audio_file.save('/')
-        filename_mp3 = 'data/records/crow.mp3'
+        dirname = 'data/records/'
+        audio_file = request.files['audio']
+        filename = secure_filename(audio_file.filename)
+        audio_file.save(dirname)
+
+        filename_mp3 = os.path.join(dirname, filename)
+        """filename_mp3 = 'data/records/crow.mp3'
         filename_tif = 'data/records/crow.tif'
         dirname = 'data/records/'
         get_sample(filename_mp3, 'crow', dirname)
         tensor = create_result(filename_tif)
-        result = get_argmax_elem_name(tensor)
+        result = get_argmax_elem_name(tensor)"""
 
-        return result
+        return filename_mp3
 
 
 @namespace.route('/fit')
